a2a0e786b2e146c585b7f69578d914ff78d483a3
Nabil Mneymneh <133052402+NabilMneymneh@users.noreply.github.com>
2025-11-16 17:29:42 +0000

    Generated by Spark: replace the logo, make the attached site logo

--- src/components/storefront/Storefront.tsx
+++ src/components/storefront/Storefront.tsx
@@ -4,7 +4,7 @@ import { ProductDetail } from './ProductDetail'
 import { CartSheet } from './CartSheet'
 import { CheckoutDialog } from './CheckoutDialog'
 import { Product } from '@/types'
-import logo from '@/assets/images/logo.svg'
+import logo from '@/assets/images/logo.png'
 
 interface StorefrontProps {
   showCart?: boolean
