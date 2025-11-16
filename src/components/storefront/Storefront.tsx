77cf40ef24df25a42743f443cb4c5e9312c89751
Nabil Mneymneh <133052402+NabilMneymneh@users.noreply.github.com>
2025-11-16 17:45:42 +0000

    Generated by Spark: Fix all reported errors.

--- src/components/storefront/Storefront.tsx
+++ src/components/storefront/Storefront.tsx
@@ -4,7 +4,7 @@ import { ProductDetail } from './ProductDetail'
 import { CartSheet } from './CartSheet'
 import { CheckoutDialog } from './CheckoutDialog'
 import { Product } from '@/types'
-import logo from '@/assets/images/Wide-White.png'
+import logoImage from '@/assets/images/Wide-White.png'
 
 interface StorefrontProps {
   showCart?: boolean
@@ -29,7 +29,7 @@ export function Storefront({ showCart = false, onCartClose }: StorefrontProps) {
     <>
       <header className="sticky top-0 bg-gradient-to-r from-primary to-secondary text-primary-foreground z-40 shadow-md">
         <div className="px-5 py-4 flex items-center justify-center">
-          <img src={logo} alt="House of Mouse" className="h-10 w-auto" />
+          <img src={logoImage} alt="House of Mouse" className="h-10 w-auto" />
         </div>
       </header>
 
